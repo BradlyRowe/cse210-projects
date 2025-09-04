dfdfe3f8a5dd7aa8f12ecde51f49aa5f7fa7ad4b
Bradly Rowe <72034221+BradlyRowe@users.noreply.github.com>
2025-09-04 14:57:51 -0600

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is you grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string grademod = "";
+        if (gradePercentage % 10 >= 7 && gradePercentage >= 60 && gradePercentage < 95)
+        {
+            grademod = "+";
+        }
+        else if (gradePercentage % 10 < 3 && gradePercentage >= 60 && gradePercentage < 95)
+        {
+            grademod = "-";
+        }
+        string letterGrade = "";
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}{grademod}");
+
     }
 }
