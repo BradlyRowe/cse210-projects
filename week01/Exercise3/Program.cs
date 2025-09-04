c89952cd6e5a1cc4553a4356bc6cd0c3da23c2d3
Bradly Rowe <72034221+BradlyRowe@users.noreply.github.com>
2025-09-04 16:14:31 -0600

    Update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,28 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = Random.Shared.Next(1, 101);
-        int guess = 0;
-        int attempts = 0;
         Console.WriteLine("Welcome to the Number Guessing Game!");
         do
         {
-            attempts++;
-            Console.Write("What is your guess? ");
-            string input = Console.ReadLine();
-            guess = int.Parse(input);
-            if (guess < magicNumber)
+        int magicNumber = Random.Shared.Next(1, 101);
+        int guess = 0;
+        int attempts = 0;
+            do
             {
-                Console.WriteLine("Too low");
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Too high");
-            }
-            else
-            {
-                Console.WriteLine($"You guessed it in {attempts} guesses!");
-            }
-        } while (guess != magicNumber);
+                attempts++;
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} guesses!");
+                }
+            } while (guess != magicNumber);
+            Console.Write("Do you want to play again (y/n)? ");
+        } while (Console.ReadLine() == "y");
     }
 }
