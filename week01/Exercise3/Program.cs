3723c6f25d11291289e84d6322d67510f771ddc2
Bradly Rowe <72034221+BradlyRowe@users.noreply.github.com>
2025-09-04 16:05:52 -0600

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = Random.Shared.Next(1, 101);
+        int guess = 0;
+        int attempts = 0;
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        do
+        {
+            attempts++;
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {attempts} guesses!");
+            }
+        } while (guess != magicNumber);
     }
 }
