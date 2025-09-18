23984f672f7d86d639e8aba7c050d24b2b5ee1ae
Bradly Rowe <72034221+BradlyRowe@users.noreply.github.com>
2025-09-18 12:47:46 -0600

    Update week 3

---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics.Contracts;
+
+public class fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public fraction(int num)
+    {
+        _top = num;
+        _bottom = 1;
+    }
+
+    public fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
